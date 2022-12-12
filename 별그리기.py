cdd0f9c1370f6e5994dbc3109d82e8076d3ecb29
수지 김 <sz.kim@cloocus.com>
2022-12-12 15:00:32 +0900

    정답

--- 별그리기.py
+++ 별그리기.py
@@ -1,8 +1,11 @@
 n = 5
-for i in range(n+3):
-    if i % 2 == 1:
-        a = "1" * i
-        # b = a[::-1]
-        print(a)
-    else:
-        pass
+a = 2*n+1
+
+for i in range(a//2):
+    print(' ' * (a//2 - i), end = '')
+    print('1' * (2*i+1))
+
+for i in range(a//2-1):
+    print(' ' * (i + 2), end = '')
+    print('1' * ((a//2*2)-3-2*i))
+

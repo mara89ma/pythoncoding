72ae3f65d170c078fd33b0fb1733e7405b6612c9
Suzie/DevSecOps/Cloud/@Cloocus <84659725+mara89ma@users.noreply.github.com>
2022-12-15 19:11:47 +0900

    Update 4. 콜라츠의 추측.py
        if n ==1:
            break 추가

--- 22-12-2/4. 콜라츠의 추측.py
+++ 22-12-2/4. 콜라츠의 추측.py
@@ -1,11 +1,17 @@
-i = int(input("입력하세요"))
+n = int(input("입력하세요"))
 c = 0
-# for i in range(N):
-while i == 1: # i가 1일때 까지 반복
-    if i % 2 == 0: # i가 짝수 이면
-        i = i/2 # i는 i를 2로 나눈 값
+for i in range(500):
+    if n % 2 == 0: # i가 짝수 이면
+        n = n/2 # i는 i를 2로 나눈 값
         c = c + 1 # 이 구문이 실행 되면 c에 1을 더함
-    elif i % 2 == 1 : # i가 홀수이면
-        i = (3*i) + 1 # 3을 곱하고 1을 더한이 i가 된다.
+    if n ==1:
+        break
+    if n % 2 == 1 : # i가 홀수이면
+        n = (3*n) + 1 # 3을 곱하고 1을 더한이 i가 된다.
         c = c + 1 # 이 구문이 실행 되면 c에 1을 더한다.
+    if n == 1:
+        break
+    if n >500 and n != 1:
+        c = -1
+        break
 print(c)

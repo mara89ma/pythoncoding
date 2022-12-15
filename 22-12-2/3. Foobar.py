f5b2dfd045cccf490093284255749c42d50b951f
Suzie/DevSecOps/Cloud/@Cloocus <84659725+mara89ma@users.noreply.github.com>
2022-12-15 16:29:35 +0900

    Update 3. Foobar.py
    출력완료

--- 22-12-2/3. Foobar.py
+++ 22-12-2/3. Foobar.py
@@ -2,15 +2,15 @@ n  = int(input("입력하세요"))
 # n>=1 and n<=50
 for i in range(1,n+1): # 1부터 n까지 출력
     # print(i)
-    if i % 3 == 0 and i % 5 == 0: # 만약 i가 3의 배수이고 5의 배수면 "FooBar" 출력
-        n = "FooBar"
-        print(n)
-    elif i % 3 == 0: # 둘다 아니고 3의 배수면
-        i = "Foo" # Foo 출력
-        print(n)
-    elif i % 5 ==0: # 둘다 아니고 5의 배수면
-        i = "Bar" # Bar 출력
-        print(n)
-    else: # 다 아니면 ? 그냥 숫자 출력
-        print(n)
+    if i % 3 == 0  and i % 5 == 0:
+        i = "FooBar"
+        print(i)
+    elif i % 3 == 0:
+        i = "Foo"
+        print(i)
 
+    elif i % 5 == 0 :
+        i = "Bar"
+        print(i)
+    else:
+        print(i)

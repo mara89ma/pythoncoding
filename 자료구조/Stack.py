8c152678ee1b668f431e0a1a83102ac4b82eae29
Suzie/DevSecOps/Cloud/@Cloocus <84659725+mara89ma@users.noreply.github.com>
2023-01-29 22:57:05 +0900

    Update Stack.py
    Test 추가

--- 자료구조/Stack.py
+++ 자료구조/Stack.py
@@ -1,3 +1,7 @@
+# 스택은 마지막에 삽입한 데이터를 가장 먼저 사용 한다.
+# 나중에 들어간 데이터가 젤 먼저 나오는 방식
+# 들어오고 나오는 방식 거꾸로
+
 class Stack: #스택 클래스를 정의한다.
     def __init__(self):
         self.data = [] # 데이터를 리스트로 정의해준다.
@@ -5,3 +9,11 @@ class Stack: #스택 클래스를 정의한다.
         self.data.append(value) # append() 맨 마지막에 값을 추가한다.
     def pop(self):
         self.data.pop() # pop()은 리스트의 맨 마지막 요소를 리턴하고 그 요소는 삭제
+
+# TEST
+S = Stack()
+S.push(12)
+S.push(13)
+print(S.data)
+S.pop()
+print(S.data)
